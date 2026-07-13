8200ed8b95979e60767464e88734e219197508b7
@joyobrata~` <joyobratadas.85912@gmail.com>
2026-07-13 19:26:43 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (52.85%) - LeetSync

---
+++ 27-remove-element/remove-element.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        
+        #updating nums 
+        j=0
+        for i in range(len(nums)):
+            if nums[i]!=val:
+                nums[j]=nums[i]
+                j=j+1
+        return j
+            
+
+                
+       
+        
