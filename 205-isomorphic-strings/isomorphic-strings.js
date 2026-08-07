89476afc0737ad3b35968c7b0fb253f715d27bb7
@joyobrata~` <joyobratadas.85912@gmail.com>
2026-08-07 23:38:36 +0530

    Time: 1 ms (99.09%) | Memory: 54.9 MB (61.90%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    let mapST = {};
+    let mapTS = {};
+
+    for (let i = 0; i < s.length; i++) {
+        let c1 = s[i];
+        let c2 = t[i];
+
+        if ((mapST[c1] && mapST[c1] !== c2) ||
+            (mapTS[c2] && mapTS[c2] !== c1)) {
+            return false;
+        }
+
+        mapST[c1] = c2;
+        mapTS[c2] = c1;
+    }
+
+    return true;
+};
